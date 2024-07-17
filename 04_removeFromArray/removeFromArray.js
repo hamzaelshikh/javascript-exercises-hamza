8272936833584ae92ec5898ca2c40f8bdd5cf47b
hamza <hamzashwliap@gmail.com>
2024-07-17 17:39:56 +0300

    finish to number 7

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(myArray,item) {
+    const index =myArray.indexof(item);
+    return myArray.splice(index,1);
+    
 };
 
 // Do not edit below this line
